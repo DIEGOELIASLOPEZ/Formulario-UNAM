b10db9e584c59323e6c6e1307f6ee6b8ea00700a
Diego Lopez <die_elias2012@hotmail.com>
2023-11-18 17:06:14 -0600

    COMMIT 5: se agregreagaon nuevos archivos HTML y una modificiacion del archivo CSS donde damos mas orden a la pagina como ademas crear una pagina de inicio, donde ira la tabla y donde te mande despues de llenar el formulario

---
+++ PHP/inicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link rel="shortcut icon" href="media/LOGO UNAM.png" type="image/x-icon"> 
+    <link rel="stylesheet" href="media/estilos.css">
+</head>
+<body>
+    <form>
+        <div class="contenedor">
+            <div class="mensaje">
+                <h1>Formulario</h1>
+            </div>
+        </div>
+    
+        <div class="botones-container">
+            <a href="formulario.html">
+                <button type="button">Iniciar</button>
+            </a>
+
+            <a href="tabla.html">
+                <button type="button">tabla</button>
+            </a>
+        </div>
+    </form>
+</body>
+</html>
