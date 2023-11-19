bca634483615b2c1b1e126217e5a14c012d4f44a
Diego <die_elais2012@hotmail.com>
2023-11-18 22:06:26 -0600

    COMMIT 9: Se logra arreglar problema con el formulario.php y estilos.cdd ademas de elimanar estilos2.css

---
+++ PHP/formulario.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Encuesta Institucional</title>
+    <link rel="stylesheet" href="media/estilos.css">
+    <style>
+        
+    </style>
+</head>
+<body>
+
+    <form>
+        <label for="numeroCuenta">Número de Cuenta:</label>
+        <input type="text" id="numeroCuenta" name="numeroCuenta" required>
+
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required>
+
+        <label for="facultad">Facultad:</label>
+        <input type="text" id="facultad" name="facultad" required>
+
+        <label for="carrera">Carrera:</label>
+        <input type="text" id="carrera" name="carrera" required>
+
+        <label for="generacion">Generación:</label>
+        <input type="text" id="generacion" name="generacion" required>
+
+        <button type="submit">Enviar</button>
+    </form>
+
+</body>
+</html>
